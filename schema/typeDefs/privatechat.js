979d6e2f6c035ebcfb63a1ff2e9b5fd578410223
Muhammad Jamil Abubakar <imuhdjml@gmail.com>
2020-11-05 16:10:31 +0100

    Privatechat typedefs updated.

--- schema/typeDefs/privatechat.js
+++ schema/typeDefs/privatechat.js
@@ -1,10 +1,24 @@
 exports.mutations = `
     sendDirectMessage(id: ID!, message: String!): Message!
-    startConversation(username: String!): String
+    startConversation(username: String!): Int!
     deleteChat(id: ID!): String
+    setCount(id: ID!): String
 `;
 
 exports.queries = `
     directMessages(id: ID!): [Message]!
-    friends: [Users!]!
+    friends: [Friend!]!
+`;
+
+exports.types = `
+    type Friend {
+        username: String!
+        message: String
+        conversationId: ID!
+        profilepic: String!
+        lastmessage: String
+        count: Int!
+        online: String
+        lastmsgTime: String
+    }
 `;
