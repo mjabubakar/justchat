332cd308679b48169378b35b34a3ef38dc9951d9
Muhammad Jamil Abubakar <imuhdjml@gmail.com>
2020-11-11 11:19:19 +0100

    commit

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -51,6 +51,7 @@ var app = express();
 var allowCrossDomain = function allowCrossDomain(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With"); // intercept OPTIONS method
 
   if ("OPTIONS" == req.method) {
